356c908eaf40b0c14bf6e8ac96d503f5853b7b6e
Michael Bailey <mikeyobviously@gmail.com>
2015-07-15 14:45:36 -0700

    Update databaseconnect.php

--- databaseconnect.php
+++ databaseconnect.php
@@ -1,4 +1,3 @@
 <?php 
-//$con=mysqli_connect('mysql.omahapokerstrategy.org','ops_dbdemo','dbdemo','ops_cms') or die("Database Error");
 $con=mysqli_connect("localhost","root","root","ops_cms");
 ?>
